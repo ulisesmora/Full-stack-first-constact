014b4751da95655d6d139c8980f3814cb9d77e6c
Enrique <enry1375@gmail.com>
2022-11-04 10:46:45 -0600

    added challenge

---
+++ README.md
@@ -0,0 +1,22 @@
+# 🏠/💻 Around Full Stack developer first contact challenge  
+*Congrats, if you are reading this,we consider you a god candidate to be part of `Around`
+we hope that you will enjoy this challenge and that it will give us a better idea of your skills.*
+
+This challenge is about your experience with TS and simple algorithms. and its not itended
+to be fully completed, we are more interested in how you approach the problem and how you
+solve it.
+
+Take a look to the index file, you will find more information about the challenge there.
+
+### These are the rules:
+* Your answers should be as simple as possible
+* To upload your answers, please fork this repo and open a PR to this repo 
+* the idea is to submit this test in a period of 48 hrs, but if you need more time, please let us know.
+
+### To have in mind:
+* we love simple, elegant and pretty code 👌🏻
+* this test will be evaluated using the latest version of `Deno`, have it in mind when you are writing your code
+* We love enforced code (Typed)
+
+We will be pending of your questions, or answers, so please feel free to ask anything. 
+**good luck!**
---
+++ index.ts
@@ -0,0 +1,62 @@
+//  1.- Create an array of n unique numbers, from 0 to N - 1, where N is the length of the array.
+//  i: 5
+//  o: [0, 1, 2, 3, 4]
+
+// YOUR
+// CODE
+// HERE
+
+//  using this previous method, generate an array of n numbers alternating them positive and negative starting with positive,
+//  then save it in a new variable and sort it.
+
+// i: [0,1,2,3,4]
+// o: [-3,-1,0,2,4]
+
+// YOUR
+// CODE
+// HERE
+
+// Consider the following code:
+
+const CUSTOMERS = [
+  {
+    customer: "John",
+    balance: 125,
+    id: "0x0001",
+  },
+  {
+    customer: "Jane",
+    balance: 100,
+    id: "0x0002",
+  },
+  {
+    customer: "Jim",
+    balance: 175,
+    id: "0x0003",
+  },
+  {
+    customer: "Jill",
+    balance: 200,
+    id: "0x0004",
+  },
+];
+
+// create a function that receives a name of a customer, and returns the balance of that customer.
+// if type of this function is not infered, type it.
+
+// EXTRA: optimize this fucnction to decrease the time complexity.
+// EXTRA 2 : optimize this function to be able to memorize the results of previous calls.
+
+// i: getBalance('John')
+// o: 125
+
+// consider the following code:
+
+const z = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const r = Math.ceil(Math.random() * 10);
+
+//  do you find any problem with this code? if so, what is the possible solution to prevent it?
+
+// YOUR
+// CODE
+// HERE
